4e2910435d7269e4ae48d345d15591866a3d5fb7
pluralitas-G750 <aucheukyan@gmail.com>
2020-06-12 11:58:00 +0800

    Update writeout.py
    * add class to writeout tasks

--- writeout.py
+++ writeout.py
@@ -10,17 +10,32 @@ class wrtout:
         self.timenow = str(np.datetime64('now')).replace(":","") #create timestamp for file
         self.fname = filename + "_" + self.timenow + ".csv" #join username with timestamp for file name
         self.fpath = os.path.join(self.savepath, self.fname)
-        print(self.fpath)
         np.savetxt(self.fpath, self.init, fmt="%1.3f", delimiter=',', header=self.head, comments=self.comms) #fmt = formatting(3 decimal places float), delimiter is comma cause csv
 
     def appendfile(self, data):
         with open(self.fpath, 'a') as filpath:
             np.savetxt(filpath, data, fmt="%1.3f", delimiter=',')
 
+class wrttask:
+    savepath = "C:\\Data"
+    head = 'Time, ID'
+    init = np.zeros([0,2])
+    comms = 'Check ID'
+
+    def __init__(self,filename):
+        self.timenow = str(np.datetime64('now')).replace(":","") #create timestamp for file
+        self.fname = filename + "_task_" + self.timenow + ".csv" #join username with timestamp for file name
+        self.fpath = os.path.join(self.savepath, self.fname)
+        np.savetxt(self.fpath, self.init, delimiter=',', header=self.head, comments=self.comms) #fmt = formatting(3 decimal places float), delimiter is comma cause csv
+    
+    def appendfile(self, data):
+        with open(self.fpath, 'a') as filpath:
+            np.savetxt(filpath, data, delimiter=',')
+
 if __name__ == "__main__":
 
-    test = wrtout("test")
-    wans = np.ones([100,13])
+    test = wrttask("test")
+    wans = "Hellow World"
     
     for i in range(100):
         test.appendfile(wans)
